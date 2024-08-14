05c3d1121201e2632c07fcfded0cc34da32e189d
bandalgomsu <q63530@naver.com>
2024-08-14 20:38:34 +0900

    init

---
+++ src/main/java/com/ruunivaccountserver/RuunivAccountServerApplication.java
@@ -0,0 +1,13 @@
+package com.ruunivaccountserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuunivAccountServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuunivAccountServerApplication.class, args);
+    }
+
+}
