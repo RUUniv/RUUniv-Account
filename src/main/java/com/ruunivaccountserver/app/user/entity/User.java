88cc6327930310db87fa3a1638b2cf240e4d1bd8
bandalgomsu <q63530@naver.com>
2024-08-15 20:13:40 +0900

    feat : User 엔티티 추가

---
+++ src/main/java/com/ruunivaccountserver/app/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.ruunivaccountserver.app.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @CreatedDate
+    private LocalDate createAt;
+
+    @LastModifiedDate
+    private LocalDate updateAt;
+}
