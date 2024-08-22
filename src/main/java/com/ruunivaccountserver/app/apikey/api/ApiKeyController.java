bcc8680aa5add27082bc99db5e45290412ed4562
bandalgomsu <q63530@naver.com>
2024-08-22 15:36:21 +0900

    refactor : ApiKey 조회 api passport를 통한 방식으로 변경

--- src/main/java/com/ruunivaccountserver/app/apikey/api/ApiKeyController.java
+++ src/main/java/com/ruunivaccountserver/app/apikey/api/ApiKeyController.java
@@ -9,11 +9,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,9 +40,9 @@ public class ApiKeyController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/api/v1/apiKeys/{userId}")
-    public ResponseEntity<List<ApiKeyInfo>> getApiKeysInfo(@PathVariable Long userId){
-        List<ApiKeyInfo> response = apiKeyService.getApiKeysInfo(userId);
+    @GetMapping("/api/v1/apiKeys")
+    public ResponseEntity<List<ApiKeyInfo>> getApiKeysInfo(@RequestHeader("passport") Long passport){
+        List<ApiKeyInfo> response = apiKeyService.getApiKeysInfo(passport);
 
         return ResponseEntity.ok(response);
     }
